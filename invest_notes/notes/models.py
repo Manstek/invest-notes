ff991c67b9a2068ab319a6fa41f390bdd05b8a93
Aleksandr Morozov <morozov460336@yandex.ru>
2025-09-19 16:23:16 +0300

    Create validator for title, add UniqueConstraint(title and owner), add Meta classes to Models Label and Note

--- invest_notes/notes/models.py
+++ invest_notes/notes/models.py
@@ -1,28 +1,42 @@
 from django.db import models
+from django.db.models import UniqueConstraint, functions
 from django.contrib.auth import get_user_model
+
+from .validators import validate_title
 
 
 User = get_user_model()
 
 
 class Label(models.Model):
-    author = models.ManyToManyField(User, verbose_name="авторы меток")
+    owner = models.ForeignKey(User, verbose_name="владелец метки",
+                              on_delete=models.CASCADE)
 
-    title = models.CharField(max_length=16, verbose_name='Метка',
-                             help_text='название метки')
+    title = models.CharField(max_length=50, verbose_name='Метка',
+                             help_text='название метки',
+                             validators=[validate_title,])
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f'Метка пользователя {self.author.username} : {self.title}'
+    class Meta:
+        verbose_name = 'метка'
+        verbose_name_plural = 'Метки'
+        ordering = ('title',)
+        default_related_name = 'labels'
+        UniqueConstraint(fields=[functions.Lower('title'), 'owner'],
+                         name='unique_label_per_user_case_insensitive')
 
 
 class Note(models.Model):
-    author = models.ForeignKey(User, related_name='notes',
-                               on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     labels = models.ManyToManyField(Label)
 
     text = models.TextField(verbose_name='Текст', help_text='Текст заметки')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'заметка'
+        verbose_name_plural = 'Заметки'
+        default_related_name = 'notes'
 
     def __str__(self):
         return (f'Заметка от пользователя {self.author.username}'
