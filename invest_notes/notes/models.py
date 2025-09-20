3aba8907de0e223df7898bf979d3e6a252c97878
Aleksandr Morozov <morozov460336@yandex.ru>
2025-09-21 02:33:35 +0300

    End doing models and install rest_framework

--- invest_notes/notes/models.py
+++ invest_notes/notes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.db.models import UniqueConstraint, functions
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
 from django.contrib.auth import get_user_model
 
 from .validators import validate_title
@@ -10,9 +11,9 @@ User = get_user_model()
 
 class Label(models.Model):
     owner = models.ForeignKey(User, verbose_name="владелец метки",
-                              on_delete=models.CASCADE)
+                              on_delete=models.CASCADE, related_name='labels')
 
-    title = models.CharField(max_length=50, verbose_name='Метка',
+    title = models.CharField(max_length=64, verbose_name='Метка',
                              help_text='название метки',
                              validators=[validate_title,])
     created_at = models.DateTimeField(auto_now_add=True)
@@ -21,9 +22,12 @@ class Label(models.Model):
         verbose_name = 'метка'
         verbose_name_plural = 'Метки'
         ordering = ('title',)
-        default_related_name = 'labels'
-        UniqueConstraint(fields=[functions.Lower('title'), 'owner'],
+        UniqueConstraint(Lower('title'), 'owner',
                          name='unique_label_per_user_case_insensitive')
+        indexes = [models.Index(fields=['owner'])]
+
+    def __str__(self):
+        return f'Метка "{self.title}" от пользователя {self.owner.username}'
 
 
 class Note(models.Model):
@@ -37,6 +41,7 @@ class Note(models.Model):
         verbose_name = 'заметка'
         verbose_name_plural = 'Заметки'
         default_related_name = 'notes'
+        indexes = [models.Index(fields=['author', 'created_at'])]
 
     def __str__(self):
         return (f'Заметка от пользователя {self.author.username}'
